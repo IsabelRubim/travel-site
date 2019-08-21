a1298d5c8a98d9f5bbf47a7f434e493fb081da59
Isabel Rubim <isabelaraujorubim@gmail.com>
2019-08-21 08:33:23 -0300

    Completed desktop and mobile header styles.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,10 +4,11 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
-    return gulp.src('./app/assets/styles/styles.css').pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    return gulp.src('./app/assets/styles/styles.css').pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
     .on('error', function(errorInfo) {
         console.log(errorInfo.toString());
         this.emit('end');
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -18,8 +18,17 @@ gulp.task('watch', function() {
     gulp.watch('./app/assets/styles/**/*.css', function() {
         gulp.start('cssInject');
     });
+
+    gulp.watch('./app/assets/scripts/**/*.js', function() {
+        gulp.start('scriptsRefresh');
+    });
 });
+
 
 gulp.task('cssInject', ['styles'], function() {
     return gulp.src('./app/temp/styles/styles.css').pipe(browserSync.stream());
-})
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+   browserSync.reload(); 
+});
